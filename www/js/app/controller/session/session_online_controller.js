91f0c939b3dab00c77ce11b4431a89c3ee78278f
mouylenglim <mouyleng@instedd.org>
2015-08-14 10:57:37 +0700

    fix problem of ip conflict when first install app

--- www/js/app/controller/session/session_online_controller.js
+++ www/js/app/controller/session/session_online_controller.js
@@ -39,6 +39,9 @@ var SessionOnlineController = {
     UserModel.create(endpoint.AUTH_URL, data, function () {
       App.redirectTo("#page-collection-list");
     }, function (x, t, m) {
+      if (!x.responseJSON.success) {
+        App.redirectTo("#page-login");
+      }
       if (t === "timeout" || t === "notmodified") {
         alert("Internet connection problem");
       }
