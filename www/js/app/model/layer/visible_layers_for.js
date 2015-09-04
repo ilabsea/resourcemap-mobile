9ce389a7b044b14e3f7bfe83ea50c23448405da5
mouylenglim <mouyleng@instedd.org>
2015-09-04 10:52:01 +0700

    fix #314 ui disabled always in form site when connection lose immediately

--- www/js/app/model/layer/visible_layers_for.js
+++ www/js/app/model/layer/visible_layers_for.js
@@ -1,15 +1,16 @@
 VisibleLayersFor = {
-  fetch: function(successCallback) {
+  fetch: function (successCallback) {
     var cId = localStorage.getItem("cId");
     var sId = localStorage.getItem("sId");
     var endpoint = getEndPoint();
     $.ajax({
-      url: endpoint.URL_SITE + cId + "/sites/" + sId + "/visible_layers_for?auth_token=" 
-          +  App.Session.getAuthToken(),
+      url: endpoint.URL_SITE + cId + "/sites/" + sId
+          + "/visible_layers_for?auth_token="
+          + App.Session.getAuthToken(),
       type: "GET",
       datatype: 'json',
       success: successCallback,
-      error: function(error) {
+      error: function (error) {
         console.log("Retriving sites from server : ", error);
       }
     });
--- www/js/app/model/membership/layer_membership.js
+++ www/js/app/model/membership/layer_membership.js
@@ -1,13 +1,16 @@
 LayerMembership = {
-  fetch: function(cId, successCallback) {
+  fetch: function (cId, successCallback) {
     var endpoint = getEndPoint();
     $.ajax({
-      url: endpoint.URL_SITE + cId + "/layer_memberships?auth_token=" + App.Session.getAuthToken(),
+      url: endpoint.URL_SITE + cId + "/layer_memberships?auth_token="
+          + App.Session.getAuthToken(),
       type: "GET",
       datatype: 'json',
       success: successCallback,
-      error: function(error) {
-        console.log("Retriving sites from server : ", error);
+      error: function (error) {
+        if (error.readyState == 0) {
+          FieldOfflineController.renderByCollectionId();
+        }
       }
     });
   }
